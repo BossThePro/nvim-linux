99fd4be4745d351b978114cbbb5971f3fba9d755
Malik Jensen <maliktimon@gmail.com>
2025-04-07 19:20:07 +0200

    Updated a few things in neovim config

--- lua/plugins/ultisnips.lua
+++ lua/plugins/ultisnips.lua
@@ -1,3 +1,8 @@
-return{
-"https://github.com/SirVer/ultisnips"
+return {"SirVer/ultisnips", 
+lazy = false, 
+init = function()
+vim.g.UltiSnipsExpandOrJumpTrigger = '<tab>'
+vim.g.UltiSnipsJumpBackwardTrigger = '<s-tab>'
+vim.g.UltiSnipsSnippetDirectories = {"~/.config/nvim/lua/snippets"}
+end
 }
--- lua/settings/ultisnips.lua
+++
@@ -1,4 +0,0 @@
--- Setting up keybinds and directory to look for snippets 
-vim.g.UltiSnipsExpandOrJumpTrigger="<Tab>" 
-vim.g.UltiSnipsJumpBackwardTrigger="<S-Tab>" 
-vim.g.UltiSnipsSnippetDirectories = {"~/.config/nvim/lua/snippets"}
